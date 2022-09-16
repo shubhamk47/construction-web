4ca0cdd5fb26d3d557e5c05866632f15437036f7
shubhamk47 <shubhamkhaparey47@gmail.com>
2022-09-16 12:34:01 +0530

    packaged frontend and restservice

--- rest-service/src/main/java/com/constructionshowcase/constructionweb/ConstructionWebApplication.java
+++ rest-service/src/main/java/com/constructionshowcase/constructionweb/ConstructionWebApplication.java
@@ -2,6 +2,9 @@ package com.constructionshowcase.constructionweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ConstructionWebApplication {
@@ -9,5 +12,15 @@ public class ConstructionWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConstructionWebApplication.class, args);
 	}
+
+	// @Configuration
+	// public class WebConfig implements WebMvcConfigurer {
+	// 	@Override
+	// 	public void addViewControllers(ViewControllerRegistry registry) {
+	// 		registry.addViewController("/").setViewName("forward:/index.html");
+	// 		registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/index.html");
+	// 		registry.addViewController("/{x:^(?!api$).*$}/**").setViewName("forward:/index.html");
+	// 	}
+	// }
 
 }
